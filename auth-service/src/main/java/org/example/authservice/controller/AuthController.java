391f72bd6502da4aaaf57a4dea541d42feb13dd1
kanhiya-sh <sharmakanhiya288@gmail.com>
2026-04-06 11:53:47 +0530

    [kanhiya] Refactor. Updated Application file

--- auth-service/src/main/java/org/example/authservice/controller/AuthController.java
+++ auth-service/src/main/java/org/example/authservice/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
